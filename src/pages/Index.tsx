cfcfe5980603dacccbf54ff61f795687aee3b8c8
Skip <skip@usecurling.com>
2026-03-19 20:11:13 +0000

    Update website images with new professional photos.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
-import Hero from '@/components/sections/Hero'
-import About from '@/components/sections/About'
+import { Hero } from '@/components/sections/Hero'
+import { About } from '@/components/sections/About'
 import Services from '@/components/sections/Services'
 import Contact from '@/components/sections/Contact'
 
